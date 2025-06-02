8d6f13547f4311c4dae3e4b8df2e926d68023a4b
ammynath <ezehadaeze20@gmail.com>
2025-06-02 17:59:30 +0100

    first commit to this directory

---
+++ objectclass/condition.js
@@ -0,0 +1,54 @@
+//conditions 
+//the input functions
+// if statements
+example
+
+let age = 18
+
+if (age < 18) {
+    console.log("underage")
+}
+
+let username = "Blessinng"
+
+if (username === "chidi") {
+    console.log("no serve 5 plates of food")
+    
+}
+
+let name = "Blessing"
+
+if (name === "Blessing") {
+    console.log("serve 5 plate of food")
+}
+//if...else
+
+let temperature = "33"
+
+if (temperature ==="37") {
+    console.log("temperature is normal")
+    
+} else {
+    console.log("temperature is low")
+}
+
+
+let jambscore = 203
+let jambage = 18
+
+if ((jambscore <= 200) (jambage <= 18)) {
+    console.log("Admission Granted")
+} else {
+    console.log("try again next year")
+}
+
+//if...else..if
+
+let time ""
+
+if (time > 12) {
+    console.log("good morning")
+} else if (time >=13) {
+    console.log("good afternoon")
+    
+}
